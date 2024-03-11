38394c3a7dac68ae4dc6d89b99b77c36babafff7
jlmlc96 <156986763+jlmlc96@users.noreply.github.com>
2024-03-10 21:43:48 -0500

    initial commit
    Signed-off-by: jlmlc96 <156986763+jlmlc96@users.noreply.github.com>

--- php/formulario.php
+++ php/formulario.php
@@ -1,29 +1,37 @@
 <?php
 
-$nombre = $_POST['nombre'];
-$telefono = $_POST['telefono'];
-$correo = $_POST['correo'];
-$mensaje = $_POST['mensaje'];
+if(isset($_POST['nombre']) && isset($_POST['telefono']) && isset($_POST['correo']) && isset($_POST['mensaje'])) {
+    $nombre = $_POST['nombre'];
+    $telefono = $_POST['telefono'];
+    $correo = $_POST['correo'];
+    $mensaje = $_POST['mensaje'];
 
-$formcontent="
-    Nombre: $nombre \n
-    Telefono: $telefono \n
-    Correo: $correo \n
-    Mensaje: $mensaje
-";
+    $formcontent="
+        Nombre: $nombre \n
+        Telefono: $telefono \n
+        Correo: $correo \n
+        Mensaje: $mensaje
+    ";
 
-$recipient = "alejandrosantillanjhanela@gmail.com, johanlinareslc@gmail.com";
+    $recipient = "alejandrosantillanjhanela@gmail.com, johanlinareslc@gmail.com";
 
-$subject = "Consulta vía Página Web CALILAB de $nombre";
+    $subject = "Consulta vía Página Web CALILAB de $nombre";
 
-$header = "From: $correo \r\n";
-$header .= "Content-Type: text/plain; charset=UTF-8";
-mail($recipient, $subject, $formcontent, $header) or die("Error!");
-header("Location: ../index.html");
+    $header = "From: $correo \r\n";
+    $header .= "Content-Type: text/plain; charset=UTF-8";
 
-
-
-/*https://www.youtube.com/watch?v=MjlIi5pa4Ec*///
-
+    if(mail($recipient, $subject, $formcontent, $header)) {
+        // Éxito: redirigir a la página de confirmación
+        header("Location: ../confirmacion.html");
+        exit();
+    } else {
+        // Error: mostrar mensaje de error
+        die("Hubo un problema al enviar el correo electrónico. Por favor, inténtalo de nuevo más tarde.");
+    }
+} else {
+    // Datos del formulario incompletos: redirigir a la página de error
+    header("Location: ../error.html");
+    exit();
+}
 
 ?>
